b9b981896f3b72e4b06beae4c4d0031d8bf85aa9
ankitzm <ankit.singh.zm@gmail.com>
2022-02-16 14:23:01 +0530

    code models for the contract

---
+++ src/assembly/model.ts
@@ -0,0 +1,35 @@
+import { u128Safe as u128 } from "as-bignum"
+import { context, RNG } from "near-sdk-as"
+
+
+// custom class decorator
+@nearBindgen
+export class Project {
+	id: u32
+	address: string
+	funds: u128
+	name: string
+	details: string
+
+	constructor(_address: string, _funds: u128, _name: string, _details:string) {
+		const rng = new RNG<u32>(1, u32.MAX_VALUE)
+		const roll = rng.next()
+		this.id = roll // random id
+		this.address = _address
+		this.funds = _funds
+		this.name = _name
+		this.details = _details
+	}
+}
+
+export class USER {
+	id: u32
+	address: string
+
+	constructor() {
+		const rng = new RNG<u32>(1, u32.MAX_VALUE) // RNG - Random Number Generator
+		const roll = rng.next()
+		this.id = roll // random id
+		this.address = context.sender // context.sender gives the user address
+	}
+}
