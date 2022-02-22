33dbf87f3128660884eafe8d4bf93441a203a9a6
ankitzm <ankit.singh.zm@gmail.com>
2022-02-22 11:29:15 +0530

    minor update

--- src/open_funding/__test__/index.unit.spec.ts
+++ src/open_funding/__test__/index.unit.spec.ts
@@ -1,30 +1,25 @@
 import * as contract from "../assembly"
 import { context, logging, u128, VMContext } from "near-sdk-as"
 
-describe("registerUSER", () => {
-	it("register a user", () => {
-		const userId1 = contract.registerUSER("abcd.testnet")
-		const userId2 = contract.registerUSER("xyz.testnet")
-		const userId3 = contract.registerUSER("asdf.testnet")
-		const userId4 = contract.registerUSER("xyz.testnet")
+// describe("registerUSER", () => {
+// 	it("register a user", () => {
+// 		log(contract.userIdList)
+// 		// log(contract.userList.getSome("asdf.testnet"))
+// 		// log(contract.userList.getSome("xyz.testnet"))
 
-		log(contract.userIdList)
-		log(contract.userList.getSome("asdf.testnet"))
-		log(contract.userList.getSome("xyz.testnet"))
-
-		const projectList = contract.allProjects()
-		log(projectList)
-		expect(contract.userIdList.length).toBeGreaterThan(0)
-	})
-})
+// 		const projectList = contract.allProjects()
+// 		log(projectList)
+// 		// expect(contract.userIdList.length).toBeGreaterThan(0)
+// 	})
+// })
 
 describe("addProject", () => {
 	it("add project", () => {
-		const usr = contract.registerUSER("test.testnet")
+		// const usr = contract.registerUSER("test.testnet")
 		// user 1 - two project
 		contract.addProject(
-			usr,
-			"projectowner1.testnet",
+			"test.testnet",
+			"test.testnet",
 			"test_project_1",
 			"some details",
 			"1000000000000000000000",
@@ -32,20 +27,20 @@ describe("addProject", () => {
 		log("first project of user 1 registered")
 
 		contract.addProject(
-			usr,
-			"projectowner1.testnet",
+			"test.testnet",
+			"test.testnet",
 			"test_project_2",
 			"some details, anything",
 			"0",
 		)
 		log("second project of user 1 registered")
 
-		const usr2 = contract.registerUSER("second.testnet")
+		// const usr2 = contract.registerUSER("second.testnet")
 
 		// user 2 - one project
 		contract.addProject(
-			usr2,
-			"projectuser2.testnet",
+			"second.testnet",
+			"second.testnet",
 			"test_project from user2",
 			"some details again",
 			"0",
@@ -53,10 +48,13 @@ describe("addProject", () => {
 
 		log(contract.getUsers())
 		log(contract.allProjects())
-		log(contract.getProjects(usr))
 		log("get project detail")
+		log(contract.projectData(contract.allProjects()[0]))
+		// log(contract.projects.getSome({})
+		log("data")
+		log(contract.projectData(543516756))
 		// log(contract.projects.getSome(543516756))
-		log(contract.donate(context.sender, 1919116659, 10))
-		expect(contract.projectIdList.length).toBeGreaterThan(0)
+		log("donate" + contract.donate(context.sender, 1919116659, 10))
+		// expect(contract.projectIdList.length).toBeGreaterThan(0)
 	})
 })
